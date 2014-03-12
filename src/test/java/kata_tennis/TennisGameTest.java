0f5f02085d4c0408f9e66767cd4a5b9587dce4de
Somkiat <somkiat.p@gmail.com>
2014-03-13 00:19:42 +0700

    Refactoring test to extract new method for create game

--- src/test/java/kata_tennis/TennisGameTest.java
+++ src/test/java/kata_tennis/TennisGameTest.java
@@ -6,73 +6,71 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest {
-	
+
 	TennisGame tennisGame = new TennisGame("Player A", "Player B");
-	
+
 	@Test
 	public void newGameShouldReturnLoveAll() throws Exception {
 		String score = tennisGame.getScore();
 		assertEquals("Love all", score);
 	}
-	
+
 	@Test
 	public void playerAWinFirstBall() throws Exception {
-		tennisGame.playerAWin();
+		createGame(1, 0);
 		String score = tennisGame.getScore();
 		assertEquals("Fifteen,Love", score);
 	}
-	
+
 	@Test
 	public void bothPlayerDrawWithFifteenShouldReturnFifteenAll() throws Exception {
-		tennisGame.playerAWin();
-		tennisGame.playerBWin();
+		createGame(1, 1);
 		String score = tennisGame.getScore();
 		assertEquals("Fifteen all", score);
 	}
-	
+
 	@Test
 	public void playerBWinInFirstBall() throws Exception {
-		tennisGame.playerBWin();
+		createGame(0, 1);
 		String score = tennisGame.getScore();
 		assertEquals("Love,Fifteen", score);
 	}
-	
+
 	@Test
 	public void playerBWinInFirstTwoBall() throws Exception {
-		tennisGame.playerBWin();
-		tennisGame.playerBWin();
+		createGame(0, 2);
 		String score = tennisGame.getScore();
 		assertEquals("Love,Thirty", score);
 	}
-	
+
 	@Test
 	public void playerAWinInFirstThreeBall() throws Exception {
-		tennisGame.playerAWin();
-		tennisGame.playerAWin();
-		tennisGame.playerAWin();
+		createGame(3, 0);
 		String score = tennisGame.getScore();
 		assertEquals("Forty,Love", score);
 	}
-	
+
 	@Test
 	public void playerAWinInFirstTwoBallAndPlayerBWinNextBallShouldReturnThirtyFifteen() throws Exception {
-		tennisGame.playerAWin();
-		tennisGame.playerAWin();
-		tennisGame.playerBWin();
+		createGame(2, 1);
 		String score = tennisGame.getScore();
 		assertEquals("Thirty,Fifteen", score);
 	}
-	
+
 	@Test
 	public void playersAreDeuce() throws Exception {
-		tennisGame.playerAWin();
-		tennisGame.playerAWin();
-		tennisGame.playerAWin();
-		tennisGame.playerBWin();
-		tennisGame.playerBWin();
-		tennisGame.playerBWin();
+		createGame(3, 3);
 		String score = tennisGame.getScore();
 		assertEquals("Deuce", score);
+	}
+
+	private void createGame(int scoreA, int scoreB) {
+		for (int i = 0; i < scoreA; i++) {
+			tennisGame.playerAWin();
+		}
+		for (int i = 0; i < scoreB; i++) {
+			tennisGame.playerBWin();
+		}
 	}
 
 }
