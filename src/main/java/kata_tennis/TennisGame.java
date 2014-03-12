27d803f58e258e0c96630c12f208b6f08e78c879
Somkiat <somkiat.p@gmail.com>
2014-03-12 23:37:26 +0700

    Resolve test with easy solution

--- src/main/java/kata_tennis/TennisGame.java
+++ src/main/java/kata_tennis/TennisGame.java
@@ -1,16 +1,21 @@
 package kata_tennis;
 
 public class TennisGame {
+	
+	int playerAScore = 0;
 
 	public TennisGame(String playerA, String playerB) {
 	}
 
 	public String getScore() {
+		if(playerAScore==1) {
+			return "Fifteen,Love";
+		}
 		return "Love all";
 	}
 
-	public void playerAWin() {
-		
+	public void playerAWin() {		
+		playerAScore++;
 	}
 
 }
